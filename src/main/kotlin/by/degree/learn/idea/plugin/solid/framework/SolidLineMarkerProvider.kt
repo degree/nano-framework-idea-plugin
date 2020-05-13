69a4ffa00954a78bb876d11ec6c985e23169b51e
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2020-05-13 10:59:21 +0200

    Move gutter icon to method definition

--- src/main/kotlin/by/degree/learn/idea/plugin/solid/framework/SolidLineMarkerProvider.kt
+++ src/main/kotlin/by/degree/learn/idea/plugin/solid/framework/SolidLineMarkerProvider.kt
@@ -6,19 +6,18 @@ import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiMethod
-import org.apache.commons.lang3.AnnotationUtils
 import org.jetbrains.uast.UMethod
 import org.jetbrains.uast.getUParentForIdentifier
-import java.util.*
 
 class SolidLineMarkerProvider : RelatedItemLineMarkerProvider() {
     override fun collectNavigationMarkers(
         element: PsiElement,
         result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>
     ) {
-        if (element is PsiMethod && AnnotationUtil.isAnnotated(element, ANNOTATION_POST_CONSTRUCT, 0))
+        val method = getUParentForIdentifier(element)
+        if (method is UMethod && AnnotationUtil.isAnnotated(method.javaPsi, ANNOTATION_POST_CONSTRUCT, 0))
         {
+
             val marker = NavigationGutterIconBuilder
                 .create(Icons.GHILL)
                 .setAlignment(GutterIconRenderer.Alignment.LEFT)
