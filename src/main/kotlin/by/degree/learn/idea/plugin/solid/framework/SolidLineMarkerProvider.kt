5022fc346eecae0897ea5e6c571cf7f769628d7e
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2020-05-13 21:55:55 +0200

    Draw ghill for fields

--- src/main/kotlin/by/degree/learn/idea/plugin/solid/framework/SolidLineMarkerProvider.kt
+++ src/main/kotlin/by/degree/learn/idea/plugin/solid/framework/SolidLineMarkerProvider.kt
@@ -6,6 +6,8 @@ import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiField
+import org.jetbrains.uast.UField
 import org.jetbrains.uast.UMethod
 import org.jetbrains.uast.getUParentForIdentifier
 
@@ -14,17 +16,19 @@ class SolidLineMarkerProvider : RelatedItemLineMarkerProvider() {
         element: PsiElement,
         result: MutableCollection<in RelatedItemLineMarkerInfo<PsiElement>>
     ) {
-        val method = getUParentForIdentifier(element)
-        if (method is UMethod && AnnotationUtil.isAnnotated(method.javaPsi, ANT_POST_CONSTRUCT, 0))
-        {
-
-            val marker = NavigationGutterIconBuilder
-                .create(Icons.GHILL)
-                .setAlignment(GutterIconRenderer.Alignment.LEFT)
-                .setTargets(element)
-                .createLineMarkerInfo(element)
-
-            result.add(marker)
+        val uElement = getUParentForIdentifier(element)
+        if (uElement is UMethod && AnnotationUtil.isAnnotated(uElement.javaPsi, ANT_POST_CONSTRUCT, 0)) {
+            result.add(markerFor(element, element))
+        } else if (uElement is UField && uElement.javaPsi is PsiField && AnnotationUtil.isAnnotated(uElement.javaPsi as PsiField, ANTS_INJECT, 0)) {
+            result.add(markerFor(element, element))
         }
+    }
+
+    private fun markerFor(element: PsiElement, targets: PsiElement): RelatedItemLineMarkerInfo<PsiElement> {
+        return NavigationGutterIconBuilder
+            .create(Icons.GHILL)
+            .setAlignment(GutterIconRenderer.Alignment.LEFT)
+            .setTargets(targets)
+            .createLineMarkerInfo(element)
     }
 }
