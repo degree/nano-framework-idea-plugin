760274d8ffa450fac1cd27c61af43146d614fab3
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2021-05-17 00:25:07 +0200

    remove usage of deprecated method

--- src/main/kotlin/by/degree/learn/idea/plugin/nano/framework/Icons.kt
+++ src/main/kotlin/by/degree/learn/idea/plugin/nano/framework/Icons.kt
@@ -3,5 +3,5 @@ package by.degree.learn.idea.plugin.nano.framework
 import com.intellij.openapi.util.IconLoader
 
 object Icons {
-    val GHILL = IconLoader.getIcon("/gutter/ghill.svg")
+    val GHILL = IconLoader.getIcon("/gutter/ghill.svg", Icons.javaClass)
 }
